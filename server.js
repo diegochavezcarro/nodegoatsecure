25651872ea71dfe7e5548ba0ba078ebc2aebf652
diegochavezcarro <diegochavezcarro@gmail.com>
2016-05-24 23:43:20 -0300

    Fix for A5

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var session = require("express-session");
 // var csrf = require('csurf');
 var consolidate = require("consolidate"); // Templating library adapter for Express
 var swig = require("swig");
-// var helmet = require("helmet");
+ var helmet = require("helmet");
 var MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
 var http = require("http");
 //var helmet = require("helmet");
-//var nosniff = require('dont-sniff-mimetype');
+var nosniff = require('dont-sniff-mimetype');
 var app = express(); // Web framework to handle routing requests
 var routes = require("./app/routes");
 var config = require("./config/config"); // Application config properties
@@ -37,7 +37,7 @@ MongoClient.connect(config.db, function(err, db) {
     }
     console.log("Connected to the database: " + config.db);
 
-    /*
+    
     // Fix for A5 - Security MisConfig
     // TODO: Review the rest of helmet options, like "xssFilter"
     // Remove default x-powered-by response header
@@ -50,7 +50,7 @@ MongoClient.connect(config.db, function(err, db) {
     app.use(helmet.noCache());
 
     // Allow loading resources only from white-listed domains
-    app.use(helmet.csp());
+    //app.use(helmet.csp);
 
     // Allow communication only on HTTPS
     app.use(helmet.hsts());
@@ -64,7 +64,7 @@ MongoClient.connect(config.db, function(err, db) {
 
     // Forces browser to only use the Content-Type set in the response header instead of sniffing or guessing it
     app.use(nosniff());
-    */
+    
 
     // Adding/ remove HTTP Headers for security
     app.use(favicon(__dirname + "/app/assets/favicon.ico"));
@@ -93,11 +93,11 @@ MongoClient.connect(config.db, function(err, db) {
         // Both mandatory in Express v4
         saveUninitialized: true,
         resave: true,
-            /*
+            
             // Fix for A5 - Security MisConfig
             // Use generic cookie name
             key: "sessionId",
-            */
+            
 
         
         // Fix for A3 - XSS
